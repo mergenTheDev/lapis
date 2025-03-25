d32b8a4a6011e2ea54d3a636dc42ff695aaedf6c
Berkay <mergenthedev@proton.me>
2025-03-25 22:35:48 +0300

    :)

---
+++ build/CMakeFiles/Lapis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Lapis.
+# This may be replaced when dependencies are built.
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Lapis Lazuli!" << std::endl;
+    return 0;
+}
