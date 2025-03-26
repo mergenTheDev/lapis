2204eee8348d5b49c5a80c756aaa5b02525f65b9
Berkay <mergenthedev@proton.me>
2025-03-26 22:35:29 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
+#include "../lib/gl/gl.h"
+#include "../lib/glfw/glfw3.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Lapis Lazuli!" << std::endl;
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Moonstone", nullptr, nullptr);
+
+    if (window == NULL) {
+        std::cout << "Failed to create Window!" << std::endl;
+        glfwTerminate();
+        return 0;
+    }
+
+    glfwMakeContextCurrent(window);
+     
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
