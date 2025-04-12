fab267626c1db731516924f43165a8aecd4f8c9c
Berkay <mergenthedev@proton.me>
2025-04-12 21:57:49 +0300

    Update shader.h

--- src/engine-core/shader/shader.h
+++ src/engine-core/shader/shader.h
@@ -1,2 +1,13 @@
 #pragma once
 
+#include "../globals.h"
+#include "../../../lib/gl/gl.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cerrno>
+
+std::string readFile(const char *path);
+GLuint LoadShader(const char *vertex, const char *fragment);
+
